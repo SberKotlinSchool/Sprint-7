22bfaee93af715a7cbaffe12588ab50ee773e13c
Яковлев Родион <ryakovlev@inpromconsult.ru>
2022-11-20 19:58:14 +0300

    orm#1

---
+++ orm/src/main/kotlin/ru/sber/orm/entity/Item.kt
@@ -0,0 +1,19 @@
+package ru.sber.orm.entity
+
+import javax.persistence.*
+
+@Entity
+class Item (
+    @Id
+    @GeneratedValue
+    var id: Long? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "drone_id")
+    var drone: Drone? = null,
+
+    @OneToOne(cascade = [CascadeType.ALL], fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id")
+    var product: Product,
+    var count: Int = 0,
+)
