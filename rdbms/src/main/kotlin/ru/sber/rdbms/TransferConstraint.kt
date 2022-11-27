0239da57b0dd0b102aca03a997e71715796c87c2
Michael <michaelmv@mail.ru>
2022-11-27 20:57:40 +0300

    Michael - решение задачи по теме "Реляционные БД"

--- rdbms/src/main/kotlin/ru/sber/rdbms/TransferConstraint.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/TransferConstraint.kt
@@ -1,7 +1,22 @@
 package ru.sber.rdbms
 
-class TransferConstraint {
+import java.sql.Connection
+
+class TransferConstraint(private val connection: Connection) {
     fun transfer(accountId1: Long, accountId2: Long, amount: Long) {
-        TODO()
+        val preparedStatement = connection.prepareStatement(
+            "" +
+                    "update account set amount = amount - ? where id = ?;" +
+                    "update account set amount = amount + ? where id = ?;"
+        )
+
+        preparedStatement.use {
+            it.setLong(1, amount)
+            it.setLong(2, accountId1)
+            it.setLong(3, amount)
+            it.setLong(4, accountId2)
+            val result = it.executeUpdate()
+            println("result = $result")
+        }
     }
 }
--- rdbms/src/main/kotlin/ru/sber/rdbms/TransferOptimisticLock.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/TransferOptimisticLock.kt
@@ -1,7 +1,57 @@
 package ru.sber.rdbms
 
-class TransferOptimisticLock {
+import java.sql.Connection
+import java.sql.SQLException
+
+class TransferOptimisticLock(private val connection: Connection) {
     fun transfer(accountId1: Long, accountId2: Long, amount: Long) {
-        TODO()
+        val accountVersion1 = getAccountVersion(connection, accountId1)
+        val accountVersion2 = getAccountVersion(connection, accountId2)
+        doAccountTransaction(connection, accountId1, accountVersion1, -amount)
+        doAccountTransaction(connection, accountId2, accountVersion2, amount)
+    }
+
+    /**
+     * Выполняем транзакцию по счёту
+     */
+    private fun doAccountTransaction(
+        connection: Connection,
+        accountId: Long,
+        version: Int,
+        amount: Long
+    ) {
+        val preparedStatement = connection.prepareStatement(
+            "update account set amount = amount + ?, version = version + 1 where id = ? and version = ?"
+        )
+
+        preparedStatement.use {
+            it.setLong(1, amount)
+            it.setLong(2, accountId)
+            it.setInt(3, version)
+
+            val result = it.executeUpdate()
+            println("result = $result")
+
+            if (result == 0) {
+                throw SQLException("Concurrent update")
+            }
+        }
+    }
+
+    /**
+     * Получаем версию счёта
+     */
+    private fun getAccountVersion(connection: Connection, accountId: Long): Int {
+        val preparedStatement = connection.prepareStatement(
+            "select * from account where id = ?"
+        )
+
+        preparedStatement.use {
+            it.setLong(1, accountId)
+            it.executeQuery().use { rs ->
+                rs.next()
+                return rs.getInt("version")
+            }
+        }
     }
 }
