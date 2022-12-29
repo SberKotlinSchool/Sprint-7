b5a6ff8210dd9ba8e89a7da3c7b2a0bcffacfbd6
Goshiq <vangog-2005@ngs.ru>
2022-12-29 17:55:36 +0300

    sprint-6: I do not give up

--- spring-data/src/main/kotlin/ru/sber/springdata/SpringDataApplication.kt
+++ spring-data/src/main/kotlin/ru/sber/springdata/SpringDataApplication.kt
@@ -5,7 +5,43 @@ import org.springframework.boot.runApplication
 
 
 @SpringBootApplication
-class SpringDataApplication
+//class SpringDataApplication: CommandLineRunner {
+class SpringDataApplication {
+
+//	@Autowired
+//	lateinit var modelRepository: ModelRepository
+//	override fun run(vararg args: String?) {
+//		val newModel = Model().also { model ->
+//			model.name = "Chaser"
+//			model.type = VehicleType.CAR
+//			model.manufacturer = Manufacturer().also {
+//				it.name = "Toyota"
+//			}
+//		}
+//
+//		val saved = modelRepository.save(newModel)
+//		val foundModel = modelRepository.findById(saved.id)
+//
+//		println("Saved and found models are equal: ${saved.id == foundModel.get().id}")
+//
+//		val allModels = modelRepository.findAll() as List<Model>
+//
+//		println("Found models: ${allModels.size}")
+//
+//		modelRepository.updateModelNameById(saved.id, "Legend")
+//
+//		val updatedModel = modelRepository.findById(saved.id)
+//
+//		println("Updated name is: ${updatedModel.get().name}")
+//
+//		val findAll = modelRepository.findAll()
+//		for (model in findAll) {
+//			modelRepository.deleteById(model.id)
+//		}
+//
+//		println("Repository after delete is empty: ${modelRepository.count() == 0L}")
+//	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringDataApplication>(*args)
