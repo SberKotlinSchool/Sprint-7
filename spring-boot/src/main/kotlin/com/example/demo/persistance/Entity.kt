27340352ee3421b44205a7b50e604c08c6443469
MorningCake <kireevds7@gmail.com>
2023-01-11 22:28:53 +0400

    SPRINT-7 spring boot

--- spring-boot/src/main/kotlin/com/example/demo/persistance/Entity.kt
+++ spring-boot/src/main/kotlin/com/example/demo/persistance/Entity.kt
@@ -1,3 +1,17 @@
 package com.example.demo.persistance
 
-class Entity
+import javax.persistence.Column
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+
+@javax.persistence.Entity
+data class Entity (
+    @Id
+    @GeneratedValue
+    var id: Long? = null,
+
+    @Column
+    var name: String? = null
+)
+
