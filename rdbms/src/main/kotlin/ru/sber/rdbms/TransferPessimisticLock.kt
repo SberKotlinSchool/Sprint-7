d1c05724be8bf44de6edca7a21fe2eb03efe6543
Anna Popova <anna.popova201909@gmail.com>
2023-01-14 19:12:38 +0300

    ready for PR

--- rdbms/src/main/kotlin/ru/sber/rdbms/TransferPessimisticLock.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/TransferPessimisticLock.kt
@@ -1,12 +1,65 @@
 package ru.sber.rdbms
 
-class TransferPessimisticLock {
-    fun transfer(accountId1: Long, accountId2: Long, amount: Long) {
-        TODO()
-    }
-}
+import ru.sber.rdbms.customException.InsufficientMoneyException
+import java.sql.Connection
+import java.sql.DriverManager
 
-fun main() {
-    val tc = TransferPessimisticLock()
-    tc.transfer(1, 2, 100)
+class TransferPessimisticLock {
+    var accountAmount: Long = 0
+    fun transfer(accountId1: Long, accountId2: Long, amount: Long) {
+        val connection = DriverManager.getConnection(
+            "jdbc:postgresql://localhost:5432/template1",
+            "postgres",
+            "postgres"
+        )
+
+        connection.use { conn ->
+            try {
+                conn.autoCommit = false
+                accountAmount = getAmount(conn, accountId1)
+
+                if (accountAmount < amount) {
+                    throw InsufficientMoneyException("Недостаточно средств на счете для перевода")
+                }
+
+                val prepareStatement =
+                    conn.prepareStatement("select * from accounts where id  in (?,? ) for update")
+                prepareStatement.use { statement ->
+                    statement.setLong(1, accountId1)
+                    statement.setLong(2, accountId2)
+                    statement.executeUpdate()
+                }
+                val prepareStatement2 = conn.prepareStatement(
+                    """
+                     update accounts set amount = amount - ? where id = ?;
+                     update accounts set amount = amount + ? where id = ?;
+                     """
+                )
+
+                prepareStatement2.use { statement ->
+                    statement.setLong(1, amount)
+                    statement.setLong(2, accountId1)
+                    statement.setLong(3, amount)
+                    statement.setLong(4, accountId2)
+                    statement.executeUpdate()
+                }
+            } catch (exception: Exception) {
+                println(exception.message)
+                conn.rollback()
+            } finally {
+                conn.autoCommit = true
+            }
+        }
+    }
+
+    private fun getAmount(conn: Connection, accountId1: Long): Long {
+        val prepareStatement = conn.prepareStatement("select amount from accounts where id = ?")
+        prepareStatement.use { statement ->
+            statement.setLong(1, accountId1)
+            statement.executeQuery().use {
+                it.next()
+                return it.getLong(1)
+            }
+        }
+    }
 }
