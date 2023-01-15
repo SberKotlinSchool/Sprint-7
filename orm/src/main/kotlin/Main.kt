faea586c62f236b5b27df71785098b5d2142a9ec
Anna Popova <anna.popova201909@gmail.com>
2023-01-15 19:50:02 +0300

    Ready for PR

--- orm/src/main/kotlin/Main.kt
+++ orm/src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
+import org.hibernate.SessionFactory
 import org.hibernate.cfg.Configuration
-import ru.sber.orm.dao.BookDao
 import ru.sber.orm.entities.Author
 import ru.sber.orm.entities.Book
 import ru.sber.orm.entities.Genre
@@ -12,7 +12,7 @@ fun main() {
         .buildSessionFactory()
 
     sessionFactory.use { sessionFactory ->
-        val dao = BookDao(sessionFactory)
+        val dao = BookDAO(sessionFactory)
 
         val book1 = Book(
             author = Author(1, "Pushkin"),
@@ -40,5 +40,48 @@ fun main() {
         val bookList = dao.findAll()
         println("все книги: $bookList")
 
+    }
+}
+
+class BookDAO(
+    private val sessionFactory: SessionFactory
+) {
+    fun save(book: Book) {
+        sessionFactory.openSession().use { session ->
+            session.beginTransaction()
+            session.save(book)
+            session.transaction.commit()
+        }
+    }
+
+    fun find(id: Long): Book? {
+        val result: Book?
+        sessionFactory.openSession().use { session ->
+            session.beginTransaction()
+            result = session.get(Book::class.java, id)
+            session.transaction.commit()
+        }
+        return result
+    }
+
+    fun find(isbn: String): Book? {
+        val result: Book?
+        sessionFactory.openSession().use { session ->
+            session.beginTransaction()
+            result =
+                session.byNaturalId(Book::class.java).using("isbn", isbn).loadOptional().orElse(null)
+            session.transaction.commit()
+        }
+        return result
+    }
+
+    fun findAll(): List<Book> {
+        val result: List<Book>
+        sessionFactory.openSession().use { session ->
+            session.beginTransaction()
+            result = session.createQuery("from Book").list() as List<Book>
+            session.transaction.commit()
+        }
+        return result
     }
 }
