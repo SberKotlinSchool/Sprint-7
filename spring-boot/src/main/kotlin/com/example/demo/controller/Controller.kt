b6777e75798030b81cc25d3f5022027267269400
Романова Ирина Дмитриевна <IDmRomanova@sberbank.ru>
2023-01-16 22:12:09 +0300

    spring boot

--- spring-boot/src/main/kotlin/com/example/demo/controller/Controller.kt
+++ spring-boot/src/main/kotlin/com/example/demo/controller/Controller.kt
@@ -1,3 +1,13 @@
 package com.example.demo.controller
 
-class Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Controller {
+
+    @GetMapping("test")
+    fun getRequest(): String {
+        return "This is result of get rest request!"
+    }
+}
