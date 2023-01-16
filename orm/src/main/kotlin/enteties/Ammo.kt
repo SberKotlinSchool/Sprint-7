560c5275a02c1642298f8ae8734b168540b0f55e
Konstantin <SirKot@yandex.ru>
2023-01-16 22:19:04 +0300

    ORM completed.

---
+++ orm/src/main/kotlin/enteties/Ammo.kt
@@ -0,0 +1,39 @@
+package enteties
+
+import javax.persistence.*
+
+@Entity
+class Ammo (
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    var id: Long = 0,
+
+    @Enumerated(value = EnumType.STRING)
+    var type: AmmoType,
+
+    @Enumerated(value = EnumType.STRING)
+    var caliber: AmmoCaliber,
+
+    var quantity: Int = 0
+) {
+    override fun toString(): String {
+        return "Ammo(type=$type, caliber=$caliber, quantity=$quantity)"
+    }
+}
+
+enum class AmmoCaliber {
+    MM_7_62,
+    MM_120,
+    MM_125,
+    MM_12_7
+}
+
+enum class AmmoType {
+    BULLET
+    , HE
+    , HEAT
+    , HESH
+    , SABOT
+    , TUR
+}
---
+++ orm/src/main/kotlin/enteties/Engine.kt
@@ -0,0 +1,31 @@
+package enteties
+
+import javax.persistence.*
+
+@Entity
+class Engine (
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    var id: Long = 0,
+
+    @Enumerated(value = EnumType.STRING)
+    var type: EngineType,
+
+    var power: Int = 0,
+
+    @Column(name = "serial_number", length = 100)
+    var serialNumber: String
+) {
+    override fun toString(): String {
+        return "Engine(type=$type, power=$power, serialNumber='$serialNumber')"
+    }
+}
+
+enum class EngineType {
+    GTD_1250,
+    B_84MS,
+    CV12,
+    AGT_1500,
+    MB_873_Ka_501
+}
