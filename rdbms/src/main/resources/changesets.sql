e50641c4c4f771a7da2106109ae0a3c3dc0bc9bb
Tatiana Dolgopolova <tdolgopol@mail.ru>
2023-01-18 09:14:30 +0600

    rdbms

--- rdbms/src/main/resources/changesets.sql
+++ rdbms/src/main/resources/changesets.sql
@@ -12,8 +12,8 @@ create table account1
 --rollback drop table account1;
 
 --changeset tdolgopolova:2
-insert into account1 (id, acc_number, acc_name, amount, version) values (1, "40701810000100001001", "счет 1", 500, 0);
-insert into account1 (id, acc_number, acc_name, amount, version) values (2, "40701810000100001002", "счет 2", 200, 0);
+insert into account1 (id, acc_number, acc_name, amount, version) values (1, '40701810000100001001', 'счет 1', 500, 0);
+insert into account1 (id, acc_number, acc_name, amount, version) values (2, '40701810000100001002', 'счет 2', 200, 0);
 
 --changeset tdolgopolova:3
 create index account1_number_indx on account1 using hash(acc_number);
