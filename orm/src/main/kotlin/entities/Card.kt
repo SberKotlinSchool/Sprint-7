8600ae6d6cc7455d5b12471b2a18701df7c77c5f
Панченко Вадим Андреевич <19816992@cab-wsm-0027036.sigma.sbrf.ru>
2023-01-20 21:37:48 +0300

    orm

---
+++ orm/src/main/kotlin/entities/Card.kt
@@ -0,0 +1,42 @@
+package entities
+
+import org.hibernate.annotations.Cascade
+import org.hibernate.annotations.CascadeType
+import org.hibernate.annotations.NaturalId
+import javax.persistence.*
+
+@Entity
+@Table(name = "cards", schema = "sberbank")
+class Card(
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    var id: Long = 0,
+
+    @NaturalId
+    @Column(name = "card_num")
+    var cardNum: Long = 0,
+
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @Cascade(CascadeType.ALL)
+    @JoinColumn(name = "deposit_id", referencedColumnName = "id")
+    var deposit: Deposit? = null,
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @Cascade(value = [CascadeType.SAVE_UPDATE, CascadeType.MERGE, CascadeType.PERSIST])
+    @JoinTable(
+        schema = "sberbank",
+        name = "cards_services",
+        joinColumns = [JoinColumn(name = "card_id", referencedColumnName = "id")],
+        inverseJoinColumns = [JoinColumn(name = "service_id", referencedColumnName = "id")]
+    )
+    var services: MutableSet<Service> = mutableSetOf(),
+
+    ) {
+    override fun toString(): String {
+        return "Card(id=$id, deposit_amount=${deposit?.amount}, cardNum=$cardNum, services=${services.map { service -> service.name }})"
+    }
+
+}
---
+++ orm/src/main/kotlin/entities/Deposit.kt
@@ -0,0 +1,23 @@
+package entities
+
+import org.hibernate.annotations.Cascade
+import org.hibernate.annotations.CascadeType
+import java.math.BigDecimal
+import javax.persistence.*
+
+@Entity
+@Table(name = "deposits", schema = "sberbank")
+class Deposit (
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    var id: Long = 0,
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "deposit")
+    @Cascade(CascadeType.ALL)
+    var card:Card? = null,
+
+    @Column(name = "amount")
+    var amount:BigDecimal = BigDecimal.ZERO
+)
