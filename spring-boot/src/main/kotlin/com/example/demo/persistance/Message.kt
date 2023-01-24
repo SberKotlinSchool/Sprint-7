7c772b86e63460bfd9c0272e3a6cb772cf974028
DudnikSlava <slavadvu@gmail.com>
2023-01-24 19:45:30 +0300

    Sprint-7. Spring Boot

---
+++ spring-boot/src/main/kotlin/com/example/demo/persistance/Message.kt
@@ -0,0 +1,32 @@
+package com.example.demo.persistance
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "MESSAGES")
+class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    var id: Long? = null
+
+    @Column
+    var message: String? = null
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Message
+
+        if (message != other.message) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return message?.hashCode() ?: 0
+    }
+
+
+}
