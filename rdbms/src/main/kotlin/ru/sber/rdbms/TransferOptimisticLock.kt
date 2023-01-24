62e16ee75e1c62950d19fe0b4274bcdd60ed0ca0
Artem Astafev <astafev.ph@gmail.com>
2023-01-24 23:53:48 +0300

    TransferOptimisticLock.kt - ok

--- rdbms/src/main/kotlin/ru/sber/rdbms/TransferOptimisticLock.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/TransferOptimisticLock.kt
@@ -1,7 +1,68 @@
 package ru.sber.rdbms
 
+import java.sql.DriverManager
+import java.sql.SQLException
+
 class TransferOptimisticLock {
     fun transfer(accountId1: Long, accountId2: Long, amount: Long) {
-        TODO()
+        val connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/db", "postgres", "postgres")
+
+        connection.use { conn ->
+            val autoCommit = conn.autoCommit
+            conn.autoCommit = false
+
+            try {
+                var version1: Long
+                var version2: Long
+
+                val statementGetAccount1 = conn.prepareStatement("SELECT * FROM account1 WHERE id = ?")
+
+                statementGetAccount1.use { statement ->
+                    statement.setLong(1, accountId1)
+                    statement.executeQuery().use { result ->
+                        result.next()
+                        result.getLong("amount")
+                            .let { if (it - amount < 0) throw SQLException("Account amount cannot be less than 0") }
+                        version1 = result.getLong("version")
+                    }
+                }
+
+                val statementGetAccount2 = conn.prepareStatement("SELECT * FROM account1 WHERE id = ?")
+                statementGetAccount2.use { statement ->
+                    statement.setLong(1, accountId2)
+                    statement.executeQuery().use { result ->
+                        result.next()
+                        version2 = result.getLong("version")
+                    }
+                }
+
+                val statementUpdateAccounts =
+                    conn.prepareStatement("UPDATE account1 SET amount = amount + ?, version = version + 1 WHERE id = ? AND version = ?")
+
+                statementUpdateAccounts.use { statement ->
+                    statement.setLong(1, -amount)
+                    statement.setLong(2, accountId1)
+                    statement.setLong(3, version1)
+                    statement.addBatch()
+
+                    statement.setLong(1, amount)
+                    statement.setLong(2, accountId2)
+                    statement.setLong(3, version2)
+                    statement.addBatch()
+
+                    val executedBatch = statement.executeBatch()
+                    if (executedBatch.isEmpty()) throw SQLException("Concurrent update")
+                }
+
+                conn.commit()
+
+            } catch (exception: SQLException) {
+                println(exception.message)
+                exception.printStackTrace()
+                conn.rollback()
+            } finally {
+                conn.autoCommit = autoCommit
+            }
+        }
     }
 }
--- rdbms/src/main/resources/init.sql
+++ rdbms/src/main/resources/init.sql
@@ -1,12 +1,15 @@
 --liquibase formatted sql
 
 --changeset rrmasgutov:init
-
-create table account1
+CREATE TABLE account1
 (
-    id bigserial constraint account_pk primary key,
-    amount int,
-    version int
-);
+    id      BIGSERIAL
+        CONSTRAINT account_pk PRIMARY KEY,
+    amount  BIGINT,
+    version BIGINT
+)
+;
 
+--changeset astafex:add_index
+CREATE INDEX account1_id_index ON account1 (id)
 
