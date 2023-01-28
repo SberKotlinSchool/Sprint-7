b177bde9c3e89b570593c5d2d88b9ab51dc6985f
Dmitry Biryukov <dmitrybiryukov4@gmail.com>
2023-01-28 15:42:33 +0300

    fix class name

--- spring-security/src/main/kotlin/ru/sber/app/endpoint/ApiController.kt
+++ spring-security/src/main/kotlin/ru/sber/app/endpoint/ApiController.kt
@@ -1,11 +1,10 @@
 package ru.sber.app.endpoint
 
 import org.springframework.http.MediaType
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
 import ru.sber.app.endpoint.dto.RequestData
-import ru.sber.app.entity.AddressBook
+import ru.sber.app.entity.ADDRESSBOOK
 import ru.sber.app.entity.UserDetailsAdapter
 import ru.sber.app.service.AddressBookService
 
@@ -16,7 +15,7 @@ import ru.sber.app.service.AddressBookService
 class ApiController(val service: AddressBookService) {
 
     @GetMapping("/list")
-    fun list(): Iterable<AddressBook> {
+    fun list(): Iterable<ADDRESSBOOK> {
         return service.getAddressBook()
     }
 
@@ -27,16 +26,16 @@ class ApiController(val service: AddressBookService) {
         if (requestData.public == true) {
             owner = 0
         }
-        service.add(AddressBook(0, requestData.firstName, requestData.lastName, requestData.city, owner), user.username)
+        service.add(ADDRESSBOOK(0, requestData.firstName, requestData.lastName, requestData.city, owner), user.username)
     }
 
     @PutMapping("/edit/{id}")
     fun update(@RequestBody requestData: RequestData, @PathVariable("id") id: Long) {
-        service.update(AddressBook(id, requestData.firstName, requestData.lastName, requestData.city, 0))
+        service.update(ADDRESSBOOK(id, requestData.firstName, requestData.lastName, requestData.city, 0))
     }
 
     @GetMapping("/view/{id}")
-    fun view(@PathVariable("id") id: Long): AddressBook {
+    fun view(@PathVariable("id") id: Long): ADDRESSBOOK {
         return service.getById(id)
     }
 
--- spring-security/src/main/kotlin/ru/sber/app/endpoint/UserController.kt
+++ spring-security/src/main/kotlin/ru/sber/app/endpoint/UserController.kt
@@ -4,13 +4,14 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import ru.sber.app.endpoint.dto.RequestData
-import ru.sber.app.entity.AddressBook
+import ru.sber.app.entity.ADDRESSBOOK
 import ru.sber.app.entity.UserDetailsAdapter
 import ru.sber.app.service.AddressBookService
 
@@ -23,7 +24,7 @@ class UserController(val service: AddressBookService) {
 
     @GetMapping("/list")
     fun list(model: Model): String {
-        model.addAttribute("notes", service.getAddressBook())
+        model["notes"] = service.getAddressBook()
         return "list"
     }
 
@@ -39,26 +40,26 @@ class UserController(val service: AddressBookService) {
         if (requestData.public == true) {
             owner = 0
         }
-        service.add(AddressBook(0, requestData.firstName, requestData.lastName, requestData.city, owner), user.username)
+        service.add(ADDRESSBOOK(0, requestData.firstName, requestData.lastName, requestData.city, owner), user.username)
         return "redirect:/app/list"
     }
 
-    @PreAuthorize("hasRole('ADMIN') or hasPermission(#id, 'ru.sber.app.entity.AddressBook','DELETE') " +
-            "or hasPermission(#id, 'ru.sber.app.entity.AddressBook','READ')")
+    @PreAuthorize("hasRole('ADMIN') or hasPermission(#id, 'ru.sber.app.entity.ADDRESSBOOK','DELETE') " +
+            "or hasPermission(#id, 'ru.sber.app.entity.ADDRESSBOOK','READ')")
     @GetMapping("/view/{id}")
     fun view(model: Model, @PathVariable("id") id: Long): String {
         model.addAttribute("note", service.getById(id))
         return "view"
     }
 
-    @PreAuthorize("hasRole('ADMIN') or hasPermission(#id, 'ru.sber.app.entity.AddressBook','DELETE')")
+    @PreAuthorize("hasRole('ADMIN') or hasPermission(#id, 'ru.sber.app.entity.ADDRESSBOOK','DELETE')")
     @PostMapping("/view/{id}")
     fun update(@ModelAttribute requestData: RequestData, @PathVariable("id") id: Long): String {
-        service.update(AddressBook(id, requestData.firstName, requestData.lastName, requestData.city, 0))
+        service.update(ADDRESSBOOK(id, requestData.firstName, requestData.lastName, requestData.city, 0))
         return "redirect:/app/view/$id"
     }
 
-    @PreAuthorize("hasRole('ADMIN') or hasPermission(#id, 'ru.sber.app.entity.AddressBook','DELETE')")
+    @PreAuthorize("hasRole('ADMIN') or hasPermission(#id, 'ru.sber.app.entity.ADDRESSBOOK','DELETE')")
     @GetMapping("/remove/{id}")
     fun remove(model: Model, @PathVariable("id") id: Long): String {
         model.addAttribute("note", service.remove(id))
