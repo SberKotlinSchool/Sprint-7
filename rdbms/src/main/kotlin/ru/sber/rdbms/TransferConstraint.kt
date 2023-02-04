f713872481e2e75cc75c9a825eb1e9a32ead96b6
SlumdogProgrammer <bolotin99@inbox.ru>
2023-02-05 01:29:42 +0300

    RDBMS - done

--- rdbms/src/main/kotlin/ru/sber/rdbms/TransferConstraint.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/TransferConstraint.kt
@@ -1,7 +1,44 @@
 package ru.sber.rdbms
 
+import java.sql.DriverManager
+import java.sql.SQLException
+
 class TransferConstraint {
+    private val connection = DriverManager.getConnection(
+        "jdbc:postgresql://localhost:5432/db",
+        "postgres",
+        "postgres"
+    )
+
     fun transfer(accountId1: Long, accountId2: Long, amount: Long) {
-        TODO()
+        connection.use { conn ->
+            val autoCommit = conn.autoCommit
+            try {
+                conn.autoCommit = false
+                val preparedStatement = connection.prepareStatement(
+                    "update account1 set amount = amount - ? where id = ?;" +
+                            "update account1 set amount = amount + ? where id = ?;"
+                )
+                preparedStatement.use { statement ->
+                    statement.setLong(1, amount)
+                    statement.setLong(2, accountId1)
+                    statement.setLong(3, amount)
+                    statement.setLong(4, accountId2)
+
+                    statement.executeUpdate()
+                }
+                conn.commit()
+            } catch (e: SQLException) {
+                println(e.message)
+                e.printStackTrace()
+                conn.rollback()
+            } finally {
+                conn.autoCommit = autoCommit
+            }
+        }
     }
+}
+
+fun main() {
+    TransferConstraint().transfer(1, 2, 500)
 }
--- rdbms/src/main/kotlin/ru/sber/rdbms/TransferPessimisticLock.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/TransferPessimisticLock.kt
@@ -1,7 +1,65 @@
 package ru.sber.rdbms
 
+import java.sql.DriverManager
+import java.sql.SQLException
+
 class TransferPessimisticLock {
+    private val connection = DriverManager.getConnection(
+        "jdbc:postgresql://localhost:5432/db",
+        "postgres",
+        "postgres"
+    )
+
     fun transfer(accountId1: Long, accountId2: Long, amount: Long) {
-        TODO()
+        connection.use { conn ->
+            val autoCommit = conn.autoCommit
+            try {
+                conn.autoCommit = false
+                val prepareStatementSelect1 = conn.prepareStatement("select * from account1 where id = ? for update")
+                prepareStatementSelect1.use { statement ->
+                    statement.setLong(1, accountId1)
+                    statement.executeQuery().use { resultSet ->
+                        resultSet.next()
+                        val oldAmount = resultSet.getLong("amount")
+                        if (oldAmount < amount) throw SQLException("Not enough money")
+                    }
+                }
+                val prepareStatementSelect2 = conn.prepareStatement("select * from account1 where id = ? for update")
+                prepareStatementSelect2.use { statement ->
+                    statement.setLong(1, accountId2)
+                    statement.executeQuery()
+                }
+
+                val prepareStatementUpdate1 =
+                    conn.prepareStatement("update account1 set amount = amount - ? where id = ?")
+                prepareStatementUpdate1.use { statement ->
+                    statement.setLong(1, amount)
+                    statement.setLong(2, accountId1)
+
+                    statement.executeUpdate()
+                }
+                val prepareStatementUpdate2 =
+                    conn.prepareStatement("update account1 set amount = amount + ? where id = ?")
+                prepareStatementUpdate2.use { statement ->
+                    statement.setLong(1, amount)
+                    statement.setLong(2, accountId2)
+
+                    statement.executeUpdate()
+                }
+
+                conn.commit()
+            } catch (exception: SQLException) {
+                println(exception.message)
+                exception.printStackTrace()
+                conn.rollback()
+            } finally {
+                conn.autoCommit = autoCommit
+            }
+        }
     }
+}
+
+fun main() {
+    TransferPessimisticLock().transfer(1, 2, 100)
+    TransferPessimisticLock().transfer(2, 1, 100)
 }
