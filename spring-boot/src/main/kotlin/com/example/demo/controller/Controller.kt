3a3d8cccedbf287831c60f8c670b9fc5b7340ef4
Stanislav Shamsheev <sashamsheev@sberbank.ru>
2023-11-30 14:42:40 +0700

    Домашняя работа Kotlin sprint7. springBoot

--- spring-boot/src/main/kotlin/com/example/demo/controller/Controller.kt
+++ spring-boot/src/main/kotlin/com/example/demo/controller/Controller.kt
@@ -1,3 +1,19 @@
 package com.example.demo.controller
 
-class Controller
+import com.example.demo.persistance.Entity
+import com.example.demo.persistance.EntityRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Controller(@Autowired val entityRepository: EntityRepository) {
+    @GetMapping("hello")
+    fun hello() = "Hello!"
+
+    @GetMapping("/{id}/view")
+    fun view(@PathVariable id: Long): Entity? {
+        return entityRepository.getById(id)
+    }
+}
