e3f15fd6bc02c01ff0a7dcb004b1b195dceb84bc
ShadowSith <rahimyanov_aleks@mail.ru>
2023-12-08 18:57:23 +0300

    Выполнено задание rdbms

--- rdbms/src/main/kotlin/ru/sber/rdbms/TransferPessimisticLock.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/TransferPessimisticLock.kt
@@ -1,7 +1,67 @@
 package ru.sber.rdbms
 
+import java.sql.DriverManager
+import java.sql.SQLException
+
 class TransferPessimisticLock {
+    private val connection = DriverManager.getConnection(
+            "jdbc:postgresql://localhost:5432/db",
+            "postgres",
+            "postgres"
+    )
+
+    private val querySelectForUpdate = "SELECT * FROM account1 WHERE id = ? FOR UPDATE;"
     fun transfer(accountId1: Long, accountId2: Long, amount: Long) {
-        TODO()
+
+        connection.use { conn ->
+            val autoCommit = conn.autoCommit
+            try {
+                conn.autoCommit = false
+                val prepareStatementSelectForUpdate1 = conn.prepareStatement(querySelectForUpdate)
+                prepareStatementSelectForUpdate1.use { statement ->
+                    statement.setLong(1, accountId1)
+
+                    statement.executeQuery().use {
+                        it.next()
+                        val actualAmount = it.getLong("amount")
+                        if (amount > actualAmount) throw SQLException("Не хватает денег на счете $accountId1")
+                    }
+                }
+                val prepareStatementSelectForUpdate2 = conn.prepareStatement(querySelectForUpdate)
+                prepareStatementSelectForUpdate2.use { statement ->
+                    statement.setLong(1, accountId2)
+                    statement.executeQuery()
+                }
+
+                val prepareStatementUpdateAccount1 =
+                        conn.prepareStatement("UPDATE account1 SET amount = amount - ? WHERE id = ?")
+                prepareStatementUpdateAccount1.use { statement ->
+                    statement.setLong(1, amount)
+                    statement.setLong(2, accountId1)
+
+                    statement.executeUpdate()
+                }
+                val prepareStatementUpdateAccount2 =
+                        conn.prepareStatement("UPDATE account1 SET amount = amount + ? WHERE id = ?")
+                prepareStatementUpdateAccount2.use { statement ->
+                    statement.setLong(1, amount)
+                    statement.setLong(2, accountId2)
+
+                    statement.executeUpdate()
+                }
+
+                conn.commit()
+            } catch (exception: SQLException) {
+                println(exception.message)
+                exception.printStackTrace()
+                conn.rollback()
+            } finally {
+                conn.autoCommit = autoCommit
+            }
+        }
     }
+}
+
+fun main() {
+    TransferPessimisticLock().transfer(6, 5,100)
 }
---
+++ rdbms/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+--liquibase formatted sql
+
+--changeset shadowsith:data
+
+insert into account1 values (1, 2000, 1), (2, 500, 1), (3, 4000, 1), (4, 3000, 1), (5, 6000, 1), (6, 200, 1)
