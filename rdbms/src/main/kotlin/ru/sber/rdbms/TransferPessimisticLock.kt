c2a1cae2e2d52bd7aa6f8329a7a5c131de530062
Anton Zhukov <devzhukov@gmail.com>
2023-12-14 20:06:03 +0300

    Sprint-7

--- rdbms/src/main/kotlin/ru/sber/rdbms/TransferPessimisticLock.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/TransferPessimisticLock.kt
@@ -1,7 +1,45 @@
 package ru.sber.rdbms
 
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+
 class TransferPessimisticLock {
     fun transfer(accountId1: Long, accountId2: Long, amount: Long) {
-        TODO()
+        val sql = """
+            SELECT amount
+            FROM account
+            WHERE id = $accountId1 FOR UPDATE;
+            
+            UPDATE account
+            SET amount = amount - $amount
+            WHERE id = $accountId1;
+            
+            UPDATE account
+            SET amount = amount + $amount
+            WHERE id = $accountId2;
+        """
+        executeUpdateWithPessimisticLock(sql)
+    }
+
+    private fun executeUpdateWithPessimisticLock(sql: String) {
+        val connection = DriverManager.getConnection(
+            "jdbc:postgresql://localhost:5432/db",
+            "postgres",
+            "postgres"
+        )
+        connection.use { conn ->
+            try {
+                conn.autoCommit = false
+                val statement = conn.createStatement()
+                statement.executeUpdate(sql)
+                conn.commit()
+            } catch (exception: SQLException) {
+                println(exception.message)
+                conn.rollback()
+            } finally {
+                conn.autoCommit = true
+            }
+        }
     }
 }
