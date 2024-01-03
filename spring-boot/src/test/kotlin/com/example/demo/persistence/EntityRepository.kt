7f2f70bad27d2b8ec4ffbb79dc79e0239da99bc8
avkononenko <avkononenko@domclick.ru>
2024-01-03 17:38:39 +0300

    Домашнее задание к спринту 6

---
+++ spring-boot/src/test/kotlin/com/example/demo/persistence/EntityRepository.kt
@@ -0,0 +1,30 @@
+package com.example.demo.persistence
+
+import com.example.demo.persistance.Entity
+import com.example.demo.persistance.EntityRepository
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase
+class EntityRepository {
+
+    @Autowired
+    private lateinit var entityRepository: EntityRepository
+
+    @Test
+    fun `findById should find entity`() {
+        // given
+        val savedEntity = entityRepository.save(Entity(name = "name"))
+
+        // when
+        val foundEntity = entityRepository.findById(savedEntity.id!!)
+
+        // then
+        assertTrue { foundEntity.get() == savedEntity }
+    }
+}
