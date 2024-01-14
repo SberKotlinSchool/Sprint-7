dc44f1cfda15eda810c0244b2a1522e3f5ad9bad
ivan <strateg8888@gmail.com>
2024-01-14 16:05:47 +0400

    spring-security hw

---
+++ spring-security/src/main/kotlin/ru/sber/api/dto/RecordResponse.kt
@@ -0,0 +1,30 @@
+package ru.sber.api.dto
+
+import ru.sber.domain.Record
+
+data class RecordResponse(
+  val id: Long,
+  val name: String,
+  val lastName: String,
+  val secondName: String,
+  val phoneNumber: String,
+  val city: String,
+  val street: String,
+  val houseNumber: Int,
+  val postcode: Int
+) {
+  companion object {
+    fun fromRecord(record: Record): RecordResponse =
+      RecordResponse(
+        record.id,
+        record.name,
+        record.lastName,
+        record.secondName,
+        record.phoneNumber,
+        record.city,
+        record.street,
+        record.houseNumber,
+        record.postcode
+      )
+  }
+}
---
+++ spring-security/src/main/kotlin/ru/sber/domain/Record.kt
@@ -0,0 +1,43 @@
+package ru.sber.domain
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "records")
+data class Record(
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  var id: Long = 0,
+
+  @Column(name = "name", length = 127)
+  var name: String = "",
+
+  @Column(name = "lastName", length = 127)
+  var lastName: String = "",
+
+  @Column(name = "secondName")
+  var secondName: String = "",
+
+  @Column(name = "phoneNumber")
+  var phoneNumber: String = "",
+
+  @Column(name = "city", length = 127)
+  var city: String = "",
+
+  @Column(name = "street", length = 127)
+  var street: String = "",
+
+  @Column(name = "house_number")
+  var houseNumber: Int = 0,
+
+  @Column(name = "post_code")
+  var postcode: Int = 0,
+
+  var username: String = ""
+)
