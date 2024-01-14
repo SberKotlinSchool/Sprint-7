6c10445ec6f7cea2c2c5ef1e8fcdcfdf52516421
Dima A <dpafonyushkin@domclick.ru>
2024-01-14 22:08:46 +0300

    spring-boot

--- spring-boot/src/main/kotlin/com/example/demo/persistance/Entity.kt
+++ spring-boot/src/main/kotlin/com/example/demo/persistance/Entity.kt
@@ -1,3 +1,14 @@
 package com.example.demo.persistance
 
-class Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.Entity
+
+@Entity
+data class Entity(
+    @Id
+    @GeneratedValue
+    var id: Long? = null,
+
+    var ammount: Long?
+)
